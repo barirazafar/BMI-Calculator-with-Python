fc611b4a328ffa1707437dd95db62e9c575e97a2
Barira Zafar <149144346+barirazafar@users.noreply.github.com>
2025-09-25 12:54:57 +0500

    Add files via upload

---
+++ BMI Calculator.py
@@ -0,0 +1,12 @@
+height= input("Enter your height in m: ")
+weight= input("Enter your weight in kg: ") 
+a=int(weight)
+b= float(height)
+bmi = a/b**2
+bmi_as_int= int(bmi)
+print(bmi_as_int)
+
+
+
+
+
